2c97f3d0ab36e06d68ed4c947047291e1a6fe1ad
kla96587 <kla96587@creighton.edu>
2021-04-02 20:24:01 -0500

    main push

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,18 @@
+rdoCharacteristics.onchange=function(){
+  console.log(`The value property of the control is ${rdoCharacteristics.value}`)
+  
+  if (rdoCharacteristics.value == 0)   // picked the first choice
+    lblMessage.value = (`I would agree that you are a optimistic person too!`)
+  else if (rdoCharacteristics.value == 1)
+      lblMessage.value = (`I would agree that you are a pesimistic person too!`)
+    else if (rdoCharacteristics.value == 2)
+      lblMessage.value = (`I would agree that you are a trusting person too!`)
+    else if (rdoCharacteristics.value == 3)
+      lblMessage.value = (`I would agree that you are a envious person too!`)
+}
 
-
-
+btnNextPage.onclick=function(){
+  ChangeForm(favExercises)
+}
 
 
--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -1,10 +1,28 @@
 var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
+var selections = []
 
 favExercises.onshow=function(){
+    selExercises.clear()
+    for (i = 0; i < exercises.length; i++) 
+        selExercises.addItem(exercises[i])
+  for (i = 0; i < coreExercises.length; i++)
+    selExercises.addItem(coreExercises[i])
+}
 
+selExercises.onchange=function(){
+  selections.push(selExercises.value)
+}
+
+btnSubmit.onclick= function() {
+    if (selections[0] == "situps" && selections[1] == "plank") {
+    lblExercise.value = (`You chose ${selections[0]} and ${selections[1]} - those are the two core exercises.`)
+  } else{
+    lblExercise.value = ("You did not pick the two core exercises.")
+}
 }
 
 
-
-
+btnNext.onclick=function(){
+ChangeForm(mobileNav)
+}
